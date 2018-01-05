244fb64275699b6940d287a4201c07d63a5a83ba
apoorva <apoorvashukla2506@gmail.com>
2018-01-05 17:51:10 +0530

    Scan & Retrieve details

--- BarcodeScanner/app/src/main/java/com/example/apoor/barcodescanner/ShowDetails.java
+++ BarcodeScanner/app/src/main/java/com/example/apoor/barcodescanner/ShowDetails.java
@@ -1,96 +1,105 @@
 package com.example.apoor.barcodescanner;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ShowDetails extends AppCompatActivity {
 
-  //  RequestQueue requestQueue;
-    //String showUrl = "http://localhost/test.php?serialno=123";
-     //TextView result;
-  RequestQueue requestQueue;
-  TextView txt;
-  TextView txt2;
-    Button search;
+    ListView listView;
+    TextView result;
 
-
-
-    //txt.setText(str);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_details);
 
-        search = (Button)findViewById(R.id.button);
-        txt = (TextView)findViewById(R.id.textView);
-       // txt2 = (TextView)findViewById(R.id.textView);
         Intent i = getIntent();
         final String str = i.getStringExtra("scansno");
 
+        //listView = (ListView) findViewById(R.id.listView);
+        result = (TextView) findViewById(R.id.textView);
+        getJSON("http://192.168.1.106/test.php?serialno="+str);
+    }
 
-        requestQueue = Volley.newRequestQueue(getApplicationContext());
-        //txt.setText(str);
 
-        search.setOnClickListener(new View.OnClickListener() {
+    private void getJSON(final String urlWebService) {
+
+        class GetJSON extends AsyncTask<Void, Void, String> {
 
             @Override
-            public void onClick(View view) {
-                String showUrl= "http://192.168.1.100/test.php?serialno="+str;
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET,
-                        showUrl, new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                       // System.out.println(response.toString());
-                        try {
-                            JSONArray stocks = response.getJSONArray("stocks");
-                            for (int i = 0; i < stocks.length(); i++) {
-                                JSONObject stock = stocks.getJSONObject(i);
-
-                                String name = stock.getString("item_name");
-                                String location = stock.getString("location");
-                                String staff = stock.getString("staff");
-
-                                txt.append(name + " " + location + " " + staff + " \n");
-                            }
-                            txt.append("===\n");
-
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        System.out.append(error.getMessage());
-
-                    }
-                });
-                requestQueue.add(jsonObjectRequest);
+            protected void onPreExecute() {
+                super.onPreExecute();
             }
-        });
+
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                try {
+                    loadIntoListView(s);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    URL url = new URL(urlWebService);
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    StringBuilder sb = new StringBuilder();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String json;
+                    while ((json = bufferedReader.readLine()) != null) {
+                        sb.append(json + "\n");
+                    }
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+            }
+        }
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+
     }
+    private void loadIntoListView(String json) throws JSONException {
+
+        JSONArray jsonArray = new JSONArray(json);
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject obj = jsonArray.getJSONObject(i);
+
+            String sno= obj.getString("sno");
+            String iname= obj.getString("item_name");
+            String location= obj.getString("location");
+            String staff= obj.getString("staff");
+
+            result.setText(" Serial Number -"+sno+"\n Name -"+iname+"\n Location -"+location+"\n Staff -"+staff);
+
+
+            // heroes[i] = obj.getString("item_name");
+        }
+        // ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, heroes);
+        //listView.setAdapter(arrayAdapter);
+    }
+
 }
